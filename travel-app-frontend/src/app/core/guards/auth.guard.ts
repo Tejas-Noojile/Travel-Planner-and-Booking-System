353c19158ad777ccbce12d226d5e23a99af7e5e1
Tejas-Noojile <153050114+Tejas-Noojile@users.noreply.github.com>
2025-10-30 13:06:00 +0530

    Add files via upload

---
+++ travel-app-frontend/src/app/core/guards/auth.guard.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../../features/auth/services/auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    if (this.authService.isLoggedIn()) {
+      return true; // User is logged in, allow access
+    } else {
+      // User is not logged in, redirect to the login page
+      this.router.navigate(['/auth/login']);
+      return false;
+    }
+  }
+}
